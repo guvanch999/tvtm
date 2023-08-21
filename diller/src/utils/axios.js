49b45b8d718762dfe8bbc85d21c2352fc44bb88f
Guwanch Durdyyew <guvanch.d.1999@gmail.com>
2023-08-21 16:20:28 +0500

    ip

--- diller/src/utils/axios.js
+++ diller/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const _axios = axios.create({
-  baseURL: "http://192.168.192.99:9186/api/",
+  // baseURL: "http://192.168.192.99:9186/api/",
+  baseURL: "http://216.250.8.128:9186/api/",
 });
 
 export default _axios;
