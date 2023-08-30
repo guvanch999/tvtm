5ec5840ad7cc4afe05f0051c4514d2ae6042a9d4
Guwanch Durdyyew <guvanch.d.1999@gmail.com>
2023-08-30 15:47:20 +0500

    hello

---
+++ dist/_nuxt/6eeb522.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[24],{640:function(n,t,e){"use strict";e.r(t);var l=e(93),o={},r=e(87),component=Object(r.a)(o,(function(){var n=this._self._c;return n("div",[n(l.d,[this._v("Dashboard")])],1)}),[],!1,null,null,null);t.default=component.exports}}]);
