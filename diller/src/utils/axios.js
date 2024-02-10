a6012bcc4854ba65d7784aa7868f541ffc08bb2a
Guwanch Durdyyew <guvanch.d.1999@gmail.com>
2024-02-10 15:10:31 +0500

    width

--- diller/src/utils/axios.js
+++ diller/src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const _axios = axios.create({
   // baseURL: "http://216.250.8.128:9186/api/",
-  baseURL: "http://localhost:9186/api/",
+  baseURL: "http://217.174.233.210:8080/api/",
 });
 
 export default _axios;
